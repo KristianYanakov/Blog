5250846c965d9176ec627a39e23f7e193b1446fa
KristianYanakov <christianyanakov@gmail.com>
2025-11-30 00:51:17 +0200

    update posts

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/createPost', (req, res) => {
   const content = req.body.content;
 
   const post = { title: title, content: content };
-    console.log(post);
+
   posts.push(post);
   res.redirect('/');
 });
@@ -33,6 +33,20 @@ app.post('/deletePost', (req, res) => {
   const id = req.body.postId;
 
   posts.splice(id, 1);
+  res.redirect('/');
+});
+
+app.get('/updatePost', (req, res) => {
+  const id = req.query.postId;
+  res.render('updatePost.ejs', { footerYear: footerYear, postId: id });
+});
+
+app.post('/updatePost', (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const id = req.body.postId;
+
+  posts[id] = { title: title, content: content };
   res.redirect('/');
 });
 
